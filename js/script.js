24821563229260a789c108241047ec56f38ee412
YaSlovar21 <stanko.tsep@yandex.ru>
2021-03-18 21:07:07 +0700

    переменные и дубль переделал

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ let jobInput = document.querySelector('.popup__input_type_about');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
 // Выберите элементы, куда должны быть вставлены значения полей
-let accaunt_name = document.querySelector('.profile__name');
-let accaunt_job = document.querySelector('.profile__about');
+let accauntName = document.querySelector('.profile__name');
+let accauntJob = document.querySelector('.profile__about');
 
 function popupOpen() {
     popup.classList.add('popup_opened');
-    nameInput.value = accaunt_name.textContent;
-    jobInput.value = accaunt_job.textContent;
+    nameInput.value = accauntName.textContent;
+    jobInput.value = accauntJob.textContent;
 }
 
 function popupClose() {
@@ -30,13 +30,13 @@ function formSubmitHandler(evt) {
                 // О том, как это делать, расскажем позже.
     
                 // Получите значение полей jobInput и nameInput из свойства value
-        let new_name = nameInput.value;
-                //console.log(new_name);
-        let new_job = jobInput.value;
+        let newName = nameInput.value;
+                //console.log(newName);
+        let newJob = jobInput.value;
     
                 // Вставьте новые значения с помощью textContent
-        accaunt_name.textContent = new_name;
-        accaunt_job.textContent = new_job;
+        accauntName.textContent = newName;
+        accauntJob.textContent = newJob;
         popupClose(); 
 }
 
