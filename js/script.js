32fbd3cc0fcdedf1e58574a88c8d8156b1d6435b
Slovar21 <stanko.tsep@yandex.ru>
2021-04-03 22:01:46 +0700

    js loaded

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
+/*
+
+callback = document.querySelector('.up-footer__call');
+popup_all = document.querySelector('#popupCards.popup');
+popup_all_close_button = document.querySelector('#popupCards .popup__button-close');
+
+callback.addEventListener('click', function(element) {
+  popup_all.classList.add('popup_opened');
+});
+
+popup_all_close_button.addEventListener('click', function(element) {
+  popup_all.classList.remove('popup_opened');
+});
+
+*/
+
+
+
+
 let popupOpenButton = document.querySelector('.profile__edit-button');
+
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__button-close');
 
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_about');
+
+
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
 // Выберите элементы, куда должны быть вставлены значения полей
@@ -47,3 +69,75 @@ popupCloseButton.addEventListener('click', popupClose);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+//Спринт 5
+
+const cardsContainer = document.querySelector('.elements');
+
+const addCardButton = document.querySelector('.popup__button-save_type_card'); //vremenno
+
+const initialCards = [
+        {
+          name: 'Архыз',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        },
+        {
+          name: 'Челябинская область',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        },
+        {
+          name: 'Иваново',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        },
+        {
+          name: 'Камчатка',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        },
+        {
+          name: 'Холмогорский район',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        },
+        {
+          name: 'Байкал',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        }
+      ];
+
+
+
+function addCard(name, link){
+        const cardTemplate = document.querySelector('#card-template').content;
+        const cardElement = cardTemplate.querySelector('.elements__item').cloneNode(true);
+
+        cardElement.querySelector('.elements__image').src = link;
+        cardElement.querySelector('.elements__heading').textContent = name;
+
+        cardElement.querySelector('.elements__like').addEventListener('click', function (evt) {
+                evt.target.classList.toggle('elements__like_liked');
+        });
+
+        cardElement.querySelector('.elements__delete-button').addEventListener('click', function (evt) {
+                const cardItem = this.closest('.elements__item');
+                cardItem.remove();
+        });
+
+        cardsContainer.append(cardElement);
+}
+
+//обработчик кнопки добавления новой карточки
+addCardButton.addEventListener('click', function() {
+        const name = document.querySelector('.popup__input_type_place');
+        const link = document.querySelector('.popup__input_type_link');
+
+        addCard(name.value, link.value);
+
+        name.value = '';
+        link.value = '';
+});
+
+
+initialCards.forEach(element => {
+        addCard(element.name, element.link);
+});
